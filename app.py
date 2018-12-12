ba8e264869b02295f639f1051d86b3babc2830bb
seankwalker <seankentonwalker@gmail.com>
2018-12-12 00:24:20 -0500

    ready for heroku?

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
+import os
+
 from flask import Flask, session, render_template, flash, request, redirect,\
-url_for
+        url_for
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from forms import QueryForm
 
 # Flask setup
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/wimpdb'
-app.config['SECRET_KEY'] = '7d4e41f27d441f27567d441f2b6176a'
+
+# Database for SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+
+# API Key for WTForm
+# TODO: ideally should take this out for prod
+app.config["SECRET_KEY"] = "7d4e41f27d441f27567d441f2b6176a"
+
+# Turn debug mode on, for development purposes (TODO: can take out in prod!)
+app.debug = True
+
+# Ensure responses aren't cached
+if app.config["DEBUG"]:
+    @app.after_request
+    def after_request(response):
+        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+        response.headers["Expires"] = 0
+        response.headers["Pragma"] = "no-cache"
+        return response
+
+# Database object
 db = SQLAlchemy(app)
+
+# Movie, Genre, Person, Appearance, Direction
+from models import *
 
 # Bootstrap setup
 bootstrap = Bootstrap(app)
 
-# index.html
+# Homepage (form)
 @app.route("/", methods=['GET'])
 def index():
 	form = QueryForm(request.form)
@@ -26,9 +50,10 @@ def process_form():
     genre = request.form["genre"]
 
     # Query the Heroku DB with SQLAlchemy (`db`)
+    movie = Movie.query.all()
+    print(movie)
+
 
 # Run Flask
 if __name__ == '__main__':
-    app.debug = True
     app.run()
-
---
+++ models.py
@@ -0,0 +1,49 @@
+from app import db
+
+class Movie(db.Model):
+    __tablename__ = "movies"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    release_year = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<Movie {self.name}>"
+
+class Genre(db.Model):
+    __tablename__ = "genres"
+
+    movie_id = db.Column(db.Integer, primary_key=True)
+    genre = db.Column(db.Text, primary_key=True)
+
+    def __repr__(self):
+        return f"<Genre {self.movie_id}, {self.genre}>"
+
+class Person(db.Model):
+    __tablename__ = "people"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    birth_year = db.Column(db.Integer)
+    death_year = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<Person {self.name}>"
+
+class Appearance(db.Model):
+    __tablename__ = "appeared"
+
+    actor_id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<Appearance {self.actor_id} appeared in {self.movie_id}>"
+
+class Direction(db.Model):
+    __tablename__ = "directed"
+
+    dir_id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, primary_key=True)
+
+    def __repr__(self):
+        return f"<Direction {self.dir_id} directed {self.movie_id}>"
