74913d5eb37015bf42f107217614c6e44d42f75e
Sanya Nijhawan <sanya301@gmail.com>
2018-12-11 23:12:52 -0500

    fixed text

--- forms.py
+++ forms.py
@@ -9,11 +9,11 @@ class MultiCheckboxField(SelectMultipleField):
     option_widget = CheckboxInput()
 
 class QueryForm(Form):
-    start_year = SelectField(u'Release year',\
+    start_year = SelectField(u'Start year',\
                choices=[('2010', '2010'), ('2011', '2011'), ('2012', '2012'), \
                         ('2013', '2013'), ('2014', '2014'), ('2015', '2015'), \
                         ('2016', '2016'), ('2017', '2017'), ('2018', '2018')])
-    end_year = SelectField(u'Release year',\
+    end_year = SelectField(u'End year',\
                choices=[('2010', '2010'), ('2011', '2011'), ('2012', '2012'), \
                         ('2013', '2013'), ('2014', '2014'), ('2015', '2015'), \
                         ('2016', '2016'), ('2017', '2017'), ('2018', '2018')])
