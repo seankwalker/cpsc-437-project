714c035abea626c11da7bf67b13ff681bb0271f1
Sanya Nijhawan <sanya301@gmail.com>
2018-12-11 20:42:53 -0500

    fixed server.py

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, session, render_template, flash, request, redirect,\
+url_for
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from forms import QueryForm
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/movies'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/wimpdb'
 app.config['SECRET_KEY'] = '7d4e41f27d441f27567d441f2b6176a'
 db = SQLAlchemy(app)
 
 bootstrap = Bootstrap(app)
 
-
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
 	form = QueryForm(request.form)
+	print('here')
 	if request.method=='POST':
 		year = request.form['year']
 		genre=request.form['genre']
-
-		if form.validate():    
-			return redirect(url_for('/'))
+		print(genre)
+		if form.validate():
+			flash(str(year) + " " + str(genre))
+			return redirect(url_for('index'))
 		else:
-			flash(MSG[0])
+			flash("All the form fields are required")
+	print('about to render')
 	return render_template('index.html', form=form)
 
 @app.route("/name/<name>")
--- forms.py
+++ forms.py
@@ -6,6 +6,13 @@ class QueryForm(Form):
     year = SelectField(u'Release year',\
                choices=[('2010', '2010'), ('2011', '2011'), ('2012', '2012'), \
                         ('2013', '2013'), ('2014', '2014'), ('2015', '2015'), \
-                        ('2016', '2016'), ('2017', '2017'), ('2018', '2018')], validators=[validators.required()])
+                        ('2016', '2016'), ('2017', '2017'), ('2018', '2018')])
     genre = SelectField(u'Genre',\
-               choices=[('adventure', 'Adventure'), ('sport', 'Sport'), ('musical', 'Musical'), ('documentary', 'Documentary'), ('fantasy', 'Fantasy'), ('family', 'Family'), ('crime', 'Crime'), ('sci-fi', 'Sci-Fi'), ('drama', 'Drama'), ('thriller', 'Thriller'), ('comedy', 'Comedy'), ('romance', 'Romance'), ('biography', 'Biography'), ('western', 'Western'), ('animation', 'Animation'), ('war', 'War'), ('mystery', 'Mystery'), ('action', 'Action'), ('history', 'History'), ('music', 'Music'), ('horror', 'Horror')])
+               choices=[('none', 'Choose one'), ('adventure', 'Adventure'), \
+               ('sport', 'Sport'), ('musical', 'Musical'), ('documentary', 'Documentary'), \
+               ('fantasy', 'Fantasy'), ('family', 'Family'), ('crime', 'Crime'), \
+               ('sci-fi', 'Sci-Fi'), ('drama', 'Drama'), ('thriller', 'Thriller'), \
+               ('comedy', 'Comedy'), ('romance', 'Romance'), ('biography', 'Biography'), \
+               ('western', 'Western'), ('animation', 'Animation'), ('war', 'War'), \
+               ('mystery', 'Mystery'), ('action', 'Action'), ('history', 'History'), ('music', 'Music'), \
+               ('horror', 'Horror')])
